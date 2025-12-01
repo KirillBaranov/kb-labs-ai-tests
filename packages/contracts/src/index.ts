efd81028456f6d2d057d60b12e84d15278095e8e
KirillBaranov <kirillBaranovJob@yandex.ru>
2025-12-01 14:00:04 +0300

    refactor(ai-tests): remove contracts package
    - Remove contracts package and all related files

--- packages/contracts/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { pluginContractsManifest } from './contract';
-export { parsePluginContracts, pluginContractsSchema } from './schema/contract.schema';
-export { contractsSchemaId, contractsVersion } from './version';
-export * from './types';
-export * from './schema';
-
--- packages/contracts/src/schema.ts
+++
@@ -1,4 +0,0 @@
-export * from './schema/api.schema';
-export * from './schema/artifacts.schema';
-export * from './schema/commands.schema';
-export * from './schema/workflows.schema';
